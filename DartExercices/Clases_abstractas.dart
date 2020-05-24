2ee4340965241e3a2b45fa2241a3732a39b693ba
Finithe-Phoenix <37785263+Finithe-Phoenix@users.noreply.github.com>
2020-05-24 18:25:46 -0500

    Clases_abstractas

---
+++ DartExercices/Clases_abstractas.dart
@@ -0,0 +1,26 @@
+//Se requiere la palabra abstract para generar una clase abstacta
+
+void main(){
+  final perro = new Perro();
+  perro.emitirSonido();
+  
+  final gato = new Gato();
+  gato.emitirSonido();
+}
+
+abstract class Animal{
+  int patas;
+  void emitirSonido();
+}
+
+class Perro implements Animal{
+  int patas;
+  int colas;
+  
+  void emitirSonido() => print('GUAUUUU!!');
+}
+
+class Gato implements Animal{
+  int patas;
+  void emitirSonido() => print('MIAUUUU!!');
+}
