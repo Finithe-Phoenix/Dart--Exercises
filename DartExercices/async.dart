7b583951956ad904dd2184113fe58d1e5ac89b5a
Finithe-Phoenix <37785263+Finithe-Phoenix@users.noreply.github.com>
2020-05-24 19:23:07 -0500

    Async

---
+++ DartExercices/async.dart
@@ -0,0 +1,12 @@
+void main() async{
+  print('Estamos a punto de pedir datos');
+  String data = await httpGet('http://api.nasa.com/alines');
+  print(data);
+  print('Ultima linea');
+}
+
+Future<String> httpGet(String url){
+  return Future.delayed(new Duration( seconds: 4),(){
+    return 'Hola mundo';
+  }); 
+}
